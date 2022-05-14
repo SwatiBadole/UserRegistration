9cc272b4c7c7db7c7e9f80115aaa08581316a26e
SwatiBadole <swatibadole1997@gmail.com>
2022-05-14 15:14:31 -0700

    Added:Last name  validation is done

--- src/main/java/com/bridgelabz/userRegistrationMain.java
+++ src/main/java/com/bridgelabz/userRegistrationMain.java
@@ -10,7 +10,8 @@ public class userRegistrationMain {
         do{
 
             System.out.println("Enter your choice: ");
-            System.out.println("1: Validate name.");
+            System.out.println("1: Validate Frist name.");
+            System.out.println("2:Validate last name");
             System.out.println("0: Exit.");
             choice = input.nextInt();
 
@@ -18,7 +19,9 @@ public class userRegistrationMain {
                 case 1:
                     UserRegistration.validateFirstName();
                     break;
-
+                case 2:
+                    UserRegistration.validateLastName();
+                    break;
                 default:
                     System.out.println("Enter valid input.");
             }
